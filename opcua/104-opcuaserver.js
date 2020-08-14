7ce933dbb39a5b348aa02e5ff3c690361004ecc0
mikakaraila <mika.karaila@valmet.com>
2020-08-14 13:58:08 +0300

    Version++

--- opcua/104-opcuaserver.js
+++ opcua/104-opcuaserver.js
@@ -132,8 +132,8 @@ module.exports = function (RED) {
             };
             server_options.buildInfo = {
                 productName: node.name.concat(" OPC UA server for node-red"),
-                buildNumber: "0.2.78",
-                buildDate: "2020-08-10T23:00:00"
+                buildNumber: "0.2.79",
+                buildDate: "2020-08-14T13:10:00"
             };
             verbose_log("Server options:" + JSON.stringify(server_options));
             server = new opcua.OPCUAServer(server_options);
