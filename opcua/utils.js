f05bf62c6a9861796a1a7080651e09ff047f7d62
mikakaraila <mika.karaila@valmet.com>
2022-01-17 19:54:29 +0200

    Fix: certificate manager argument for PKI folder.

--- opcua/utils.js
+++ opcua/utils.js
@@ -6,21 +6,53 @@ const config = envPaths("node-red-opcua").config;
 
 
 let _g_certificateManager = null;
-function createCertificateManager(autoAccept) {
-    if (_g_certificateManager) {
-        return _g_certificateManager;
+function createCertificateManager(autoAccept, folderName) {
+    if (folderName && folderName.length > 0) {
+        if (path.isAbsolute(folderName)) {
+            folder = folderName;
+        }
+        else {
+            folder = config.replace("Config", folderName);
+        }
+        return new opcua.OPCUACertificateManager({
+            name: "PKI",
+            rootFolder: path.join(folder, "PKI"),
+            automaticallyAcceptUnknownCertificate: autoAccept
+        });
     }
+    else {
+        folder = config;
+        if (_g_certificateManager) {
+            return _g_certificateManager;
+        }
+    }
+
     return _g_certificateManager = new opcua.OPCUACertificateManager({
         name: "PKI",
-        rootFolder: path.join(config, "PKI"),
+        rootFolder: path.join(folder, "PKI"),
         automaticallyAcceptUnknownCertificate: autoAccept
     });
 }
 let _g_userCertificateManager = null;
-function createUserCertificateManager(autoAccept) {
-    if (_g_userCertificateManager) 
-     return _g_userCertificateManager;
-
+function createUserCertificateManager(autoAccept, folderName) {
+     if (folderName && folderName.length > 0) {
+        if (path.isAbsolute(folderName)) {
+            folder = folderName;
+        }
+        else {
+            folder = config.replace("Config", folderName);
+        }
+        return new opcua.OPCUACertificateManager({
+            name: "PKI",
+            rootFolder: path.join(folder, "PKI"),
+            automaticallyAcceptUnknownCertificate: autoAccept
+        });
+    }
+    else {
+        folder = config;
+        if (_g_userCertificateManager)
+         return _g_userCertificateManager;
+    }
     return _g_userCertificateManager = new opcua.OPCUACertificateManager({
         name: "UserPKI",
         rootFolder: path.join(config, "UserPKI"),
