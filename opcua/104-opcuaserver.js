d3c335ddff1037d45713ba269659e623f10a13c3
mikakaraila <mika.karaila@valmet.com>
2022-06-29 21:47:14 +0300

    Version++

--- opcua/104-opcuaserver.js
+++ opcua/104-opcuaserver.js
@@ -307,8 +307,8 @@ module.exports = function (RED) {
             };
             
             node.server_options.buildInfo = {
-                buildNumber: "0.2.281",
-                buildDate: "2022-06-21T10:28:00"
+                buildNumber: "0.2.282",
+                buildDate: "2022-06-29T21:46:00"
             };
             
             var hostname = os.hostname();
